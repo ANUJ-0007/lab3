d19e3734d2f6cdb586f913aef50919a7449a95dc
Anuj Poudel <anujpoudel550@gmail.com>
2025-06-05 07:55:54 +0545

    initial commit

---
+++ sinhle.java
@@ -0,0 +1,21 @@
+// Superclass
+class Animal {
+    public void eat() {
+        System.out.println("Animal eats food");
+    }
+}
+
+class Dog extends Animal {
+    public void bark() {
+        System.out.println("Dog barks");
+    }
+}
+
+
+public class sinhle {
+    public static void main(String[] args) {
+        Dog d = new Dog();
+        d.eat();  
+        d.bark();  
+    }
+}
